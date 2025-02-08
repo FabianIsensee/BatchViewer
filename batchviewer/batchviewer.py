533a62816ea0d1865ecc4f48c5264da7429ea67b
Fabian Isensee <f.isensee@dkfz.de>
2025-02-08 14:00:37 +0100

    improved a lot of stuff

--- batchviewer/batchviewer.py
+++ batchviewer/batchviewer.py
@@ -1,22 +1,12 @@
-# Copyright 2017 Fabian Isensee
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-from PyQt5.QtCore import QRect
-from PyQt5.QtGui import QPainter
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QGridLayout, QApplication
-import pyqtgraph as pg
 import sys
 import numpy as np
+import pyqtgraph as pg
+
+from PyQt5.QtCore import QRect, Qt, QRectF
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import (
+    QWidget, QHBoxLayout, QGridLayout, QApplication, QLabel, QVBoxLayout
+)
 
 
 class ImageViewer2DWidget(QWidget):
@@ -27,9 +17,10 @@ class ImageViewer2DWidget(QWidget):
         self.init(width, height)
 
     def init(self, width, height):
-        self.rect = QRect(0, 0, width, height)
-        self.imageItem = pg.ImageItem()
-        self.imageItem.setImage(None)
+        # self.rect = QRect(0, 0, width, height)  # <-- REMOVED
+        # Disable auto leveling
+        self.imageItem = pg.ImageItem(autoLevels=False)
+        self.imageItem.setAutoDownsample(True)
 
         self.graphicsScene = pg.GraphicsScene()
         self.graphicsScene.addItem(self.imageItem)
@@ -38,19 +29,35 @@ class ImageViewer2DWidget(QWidget):
         self.graphicsView.setRenderHint(QPainter.Antialiasing)
         self.graphicsView.setScene(self.graphicsScene)
 
+        # Optionally fix the widget's size to something approximate:
+        self.setFixedSize(width, height)
+
         layout = QHBoxLayout()
         layout.addWidget(self.graphicsView)
         self.setLayout(layout)
-        self.setMaximumSize(width, height)
-        self.setMinimumSize(width - 10, height - 10)
 
     def setImage(self, image):
-        assert len(image.shape) == 2 or len(image.shape) == 3
-        if len(image.shape) == 4:
-            assert image.shape[-1] == 4
+        """
+        image: 2D or 3D (with last dimension =4 for RGBA).
+        """
+        assert len(image.shape) in (2, 3)
         self.image = image
-        self.imageItem.setImage(self.image)
-        self.imageItem.setRect(self.rect)
+
+        # CHANGED: Set the data without autoLevels
+        self.imageItem.setImage(self.image, autoLevels=False)
+
+        # CHANGED: Use the image shape to define the bounding rectangle
+        # For a 2D array, shape == (height, width).
+        # For e.g. RGBA, shape == (height, width, 4), so we only take the first 2 dims.
+        h, w = image.shape[:2]
+        self.imageItem.setRect(QRectF(0, 0, w, h))
+
+        # CHANGED: Lock aspect ratio (so squares remain square)
+        self.graphicsView.setAspectLocked(True)
+
+        # CHANGED: Make sure we see the entire image item
+        self.graphicsView.setRange(self.imageItem.boundingRect(), padding=0.0)
+
         if self.lut is not None:
             self.imageItem.setLookupTable(self.lut)
 
@@ -59,34 +66,56 @@ class ImageViewer2DWidget(QWidget):
 
     def setLUT(self, lut):
         self.lut = lut
+        self.imageItem.setLookupTable(self.lut)
 
 
-class ImageSlicingWidget(ImageViewer2DWidget):
+class ImageSlicingWidget(QWidget):
     def __init__(self, width=300, height=300):
+        super().__init__()
         self.slice = 0
-        super().__init__(width, height)
+        self.image3D = None
+        self.lut = None
+
+        # Set up child widgets
+        self.viewer = ImageViewer2DWidget(width, height)
+        self.label_slice = QLabel("Slice 0/0")
+
+        vlayout = QVBoxLayout()
+        vlayout.addWidget(self.viewer)
+        vlayout.addWidget(self.label_slice, alignment=Qt.AlignCenter)
+        self.setLayout(vlayout)
 
     def setImage(self, image):
         assert len(image.shape) == 3 or len(image.shape) == 4
-        if len(image.shape) == 4:
-            assert image.shape[-1] == 4
-        self.image3D = np.array(image)
-        self._updateImageSlice()
+        self.image3D = image
+        self.setSlice(self.image3D.shape[0] // 2)  # default: middle slice
 
     def _updateImageSlice(self):
-        self.imageItem.setImage(self.image3D[self.slice])
-        self.imageItem.setRect(self.rect)
+        if self.image3D is None:
+            return
+        slice_index = np.clip(self.slice, 0, self.image3D.shape[0] - 1)
+        self.viewer.setImage(self.image3D[slice_index])
         if self.lut is not None:
-            self.imageItem.setLookupTable(self.lut)
+            self.viewer.setLUT(self.lut)
+        total_slices = self.image3D.shape[0]
+        self.label_slice.setText(f"Slice {slice_index + 1}/{total_slices}")
 
-    def setSlice(self, slice):
-        slice = np.max((0, slice))
-        slice = np.min((slice, self.image3D.shape[0] - 1))
-        self.slice = slice
+    def setSlice(self, slice_index):
+        if self.image3D is None:
+            return
+        slice_index = np.clip(slice_index, 0, self.image3D.shape[0] - 1)
+        self.slice = slice_index
         self._updateImageSlice()
 
     def getSlice(self):
         return self.slice
+
+    def setLevels(self, levels, update=True):
+        self.viewer.setLevels(levels, update)
+
+    def setLUT(self, lut):
+        self.lut = lut
+        self.viewer.setLUT(lut)
 
 
 class BatchViewer(QWidget):
@@ -99,38 +128,34 @@ class BatchViewer(QWidget):
         self._init_gui()
 
     def setBatch(self, batch, lut={}):
-        assert len(batch.shape) == 4
-        batch = np.copy(batch)
+        assert len(batch.shape) == 4 or len(batch.shape) == 5
         for v in self.slicingWidgets.values():
             self._my_layout.removeWidget(v)
+            v.deleteLater()
         self.slicingWidgets = {}
-
-        if not isinstance(lut, dict):
-            lut = {i: lut for i in range(self.batch.shape[0])}
-
-        for b in range(batch.shape[0]):
-            mn = batch[b].min()
-            mx = batch[b].max()
-            if batch.dtype in (int, np.uint8, np.uint16, np.uint32, np.uint64, np.int8, np.int16, np.int32, np.int64):
-                vals = np.linspace(mn, mx, num=mx - mn + 1, dtype=batch.dtype)
-            else:
-                vals = np.linspace(mn, mx, num=255, dtype=batch.dtype)
-            max_allowed = np.prod(batch[b].shape, dtype=np.int64) // 10
-            vals = vals[:min(max_allowed, len(vals))]
-            for slice in range(batch[b].shape[0]):
-                batch[b, slice].ravel()[:len(vals)] = vals
-
         self.batch = batch
-        num_col = int(np.ceil(np.sqrt(self.batch.shape[0])))
+
+        # If lut is a single LUT array, or None, handle that
+        if not isinstance(lut, dict):
+            single_lut = lut
+            lut = {i: single_lut for i in range(batch.shape[0])}
+
+        num_volumes = self.batch.shape[0]
+        num_col = int(np.ceil(np.sqrt(num_volumes)))
         col = 0
         row = 0
-        for i in range(self.batch.shape[0]):
+
+        for i in range(num_volumes):
             w = ImageSlicingWidget(self.width, self.height)
-            if lut is not None and i in lut.keys():
+            if i in lut.keys() and lut[i] is not None:
                 w.setLUT(lut[i])
-            w.setImage(self.batch[i])
-            # w.setLevels([self.batch[i].min(), self.batch[i].max()])
-            w.setSlice(self.batch.shape[1] // 2)
+            vol = self.batch[i]
+            w.setImage(vol)
+
+            # Critical: set levels from entire volume
+            mn, mx = vol.min(), vol.max()
+            w.setLevels([mn, mx], update=True)
+
             self._my_layout.addWidget(w, row, col)
             col += 1
             if col >= num_col:
@@ -144,10 +169,16 @@ class BatchViewer(QWidget):
         self.setLayout(self._my_layout)
         self.setWindowTitle("Batch Viewer")
 
-    def wheelEvent(self, QWheelEvent):
+    def wheelEvent(self, event):
+        delta = np.sign(event.angleDelta().y())
+        modifiers = QApplication.keyboardModifiers()
+        if modifiers & Qt.ControlModifier:
+            step = 5
+        else:
+            step = 1
         for v in self.slicingWidgets.values():
-            offset = np.sign(QWheelEvent.angleDelta().y())
-            v.setSlice(v.getSlice() + np.sign(offset))
+            current_slice = v.getSlice()
+            v.setSlice(current_slice + step * delta)
 
 
 def view_batch(*args, width=500, height=500, lut={}):
@@ -164,8 +195,10 @@ def view_batch(*args, width=500, height=500, lut={}):
                 pass
             while len(item.shape) < 4:
                 item = item[None]
+            if item.dtype == bool:
+                item = item.astype(np.uint8, copy=False)
             use_these[i] = item
-        use_these = np.concatenate(use_these, 0)
+        use_these = np.vstack(use_these)
     else:
         while len(use_these.shape) < 4:
             use_these = use_these[None]
@@ -173,58 +206,60 @@ def view_batch(*args, width=500, height=500, lut={}):
     global app
     app = QApplication.instance()
     if app is None:
-        app = QApplication(sys.argv)
+        app = QApplication([])
+
     sv = BatchViewer(width=width, height=height)
     sv.setBatch(use_these, lut)
     sv.show()
-    app.exit(app.exec_())
+
+    try:
+        from IPython import get_ipython
+        ip = get_ipython()
+        if ip is not None and ip.has_trait('kernel'):
+            ip.magic('gui qt')
+        else:
+            app.exec_()
+    except ImportError:
+        app.exec_()
 
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+    # Example usage
+    # app = QApplication.instance()
+    # if app is None:
+    #     app = QApplication(sys.argv)
+    #
+    # # random data: 6 volumes, each 100 slices, 100 x 100 in-plane
+    # batch = np.random.uniform(0, 3, (6, 100, 100, 100)).astype(float)
+    # batch[:, 50:] *= 0.5
+    #
+    # # example LUT dictionary for volumes #1 and #2
+    # # each LUT is Nx4 (RGBA). Values should be in [0,255] for pyqtgraph
+    # lut_dict = {
+    #     1: np.array([[0, 0, 0, 255],      # black
+    #                  [255, 0, 0, 255],    # red
+    #                  [0, 255, 0, 255],    # green
+    #                  [0, 0, 255, 255]],   # blue
+    #                 dtype=np.uint8),
+    #     2: np.array([[255, 255, 0, 255],
+    #                  [0, 255, 255, 255],
+    #                  [255, 0, 255, 255],
+    #                  [128, 128, 128, 255]],
+    #                 dtype=np.uint8),
+    # }
+    #
+    # viewer = BatchViewer(width=400, height=400)
+    # viewer.setBatch(batch, lut_dict)
+    # viewer.show()
+    #
+    # sys.exit(app.exec_())
 
-    global app
-    app = QApplication.instance()
-    if app is None:
-        app = QApplication(sys.argv)
-    sv = BatchViewer()
-    batch = np.random.uniform(0, 3, (6, 100, 100, 100)).astype(int)
-    lut = {2: np.array([[0, 0.5, 0, 1], [0, 0, 0.5, 1], [0.5, 0, 0, 1], [0.5, 0.5, 0, 1]]) * 255,
-           1: np.array([[1, 0.5, 0, 1], [0, 0, 0.5, 1], [0.5, 0, 0, 1], [0.5, 0.5, 0, 1]]) * 255}
-    sv.setBatch(batch, lut)
-    sv.show()
-    app.exec_()
-    app.deleteLater()
-    sys.exit()
-    # IPython.embed()
-
-'''class SliceViewer(QtGui.QWidget):
-    def __init__(self):
-        super(SliceViewer, self).__init__()
-        self.initUI()
-
-    def wheelEvent(self, event):
-        for v in [self.imageViewer, self.imageViewer2]:
-            v.setSlice(v.getSlice() + event.delta()/120)
-
-    def initUI(self):
-        image1 = np.random.uniform(-0.5, 255., (100, 100, 100)).astype(np.float32)
-        image2 = np.random.uniform(-100., 255., (100, 100, 100)).astype(np.float32)
-
-        self.imageViewer = ImageSlicingWidget()
-        self.imageViewer2 = ImageSlicingWidget()
-
-        self.imageViewer.setImage(image1)
-        self.imageViewer2.setImage(image2)
-
-        hLayout = QtGui.QHBoxLayout()
-        hLayout.addWidget(self.imageViewer)
-        hLayout.addWidget(self.imageViewer2)
-        hLayout.addStretch()
-
-        self.setLayout(hLayout)
-
-        #self.setGeometry(0, 0, 1200, 1200)
-        self.setWindowTitle('QtGui.QCheckBox')
-
-        self.show()'''
+    a = np.zeros((64, 64, 64))
+    # a[32:42, 32:42, 32:42] = 1
+    # a[37:47, 37:47, 37:47] = 2
+    a[:, 32, 32] = 2
+    a[:, -1, :] = 1
+    a[:, :, -1] = 1
+    a[:, 0, :] = 1
+    a[:, :, 0] = 1
+    view_batch(a, width=500, height=500)
